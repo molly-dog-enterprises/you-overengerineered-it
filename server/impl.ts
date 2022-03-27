c194a16816ec319ef9ed1002bca83c2d6ad02e00
David Henry <david@decoybecoy.com>
2022-03-27 15:02:56 +0100

    User can join game

--- server/impl.ts
+++ server/impl.ts
@@ -14,26 +14,85 @@ import {
   IEndTurnRequest,
 } from "../api/types";
 
-type InternalState = PlayerState;
+type GameState = {
+  name: string,
+  players: PlayerState[],
+  winner: UserId | null,
+  round: number,
+  maxCardLevel: number,
+  eventBus: string,
+  roundRemaining: number
+}
+type InternalState = {
+  games: GameState[],
+  playerGames: Map<UserId, GameState>
+  playerState: Map<UserId, PlayerState>
+}
 
 export class Impl implements Methods<InternalState> {
   initialize(userId: UserId, ctx: Context): InternalState {
     return {
-      deck: [],
-      lives: 0,
-      cash: 0,
-      selectedCard: CardState.default(),
-      pickableCards: [],
-      opponentPickableCards: [],
-      attachBonus: 0,
-      defenceBonuse: 0,
+      games: [],
+      playerGames: new Map<UserId, GameState>(),
+      playerState: new Map<UserId, PlayerState>(),
     };
   }
   joinGame(state: InternalState, userId: UserId, ctx: Context, request: IJoinGameRequest): Response {
-    return Response.error("Not implemented");
+    // check if player is already in a game
+    if(state.playerGames.get(userId)) {
+      return Response.error("User is already in a Game"); 
+    }
+
+    let game;
+    game = state.games.find((game) => game.name === request.name);
+    if(game === undefined) {
+      game = {
+        name: request.name,
+        players: [],
+        winner: null,
+        round: 0,
+        roundRemaining: 0,
+        maxCardLevel: 1,
+        eventBus: "pending"
+      };
+      state.games.push(game);
+    }
+
+    if(game.players.length >= 2) {
+      return Response.error("Game is already full"); 
+    }
+
+    state.playerGames.set(userId, game);
+
+    let player = PlayerState.default()
+    player.userId = userId
+    player.gameName = request.name
+
+    game.players.push(player);
+    state.playerState.set(userId, player);
+
+    // enrol player to event bus
+    // game.eventBus.subscribe('player', playerActions(player));
+
+    return Response.ok();  
   }
   startGame(state: InternalState, userId: UserId, ctx: Context, request: IStartGameRequest): Response {
-    return Response.error("Not implemented");
+    let game = state.playerGames.get(userId);
+    if(! game) {
+      return Response.error("User is is not in a game"); 
+    }
+
+    if(game.players.length != 2) {
+      return Response.error("Not enough players to state game"); 
+    }
+
+    // trigger round start to player
+    // game.eventBus.publish('player', {players: 'all', action: 'refreshPickable'});
+
+    game.round = 1;
+    game.roundRemaining = 600.0
+
+    return Response.ok();  
   }
   pickCard(state: InternalState, userId: UserId, ctx: Context, request: IPickCardRequest): Response {
     return Response.error("Not implemented");
@@ -45,6 +104,6 @@ export class Impl implements Methods<InternalState> {
     return Response.error("Not implemented");
   }
   getUserState(state: InternalState, userId: UserId): PlayerState {
-    return state;
+    return state.playerState.get(userId) || PlayerState.default();
   }
 }
